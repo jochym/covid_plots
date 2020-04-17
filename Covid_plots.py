d5571dd417870279786a81ba3aa958cf99046fb0
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-04-17 10:56:54 +0200

    Clean up, add trajectory plot

--- Covid_plots.py
+++ Covid_plots.py
@@ -16,7 +16,6 @@
 
 # %%
 # %pylab inline
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
@@ -27,22 +26,23 @@ import matplotlib.ticker as ticker
 LOGY=True
 
 # Select countries
-#selcnt = ['Poland','Sweden','United Kingdom', 'US', 'Norway', 'Germany']
-selcnt = ['Poland','Sweden','US','Germany','Norway','Italy','Spain']
-#selcnt = ['Poland', 'Germany', 'Sweden', 'Norway', 'Korea, South', 'China', 'US']
-#selcnt = ['Poland', 'Germany', 'Sweden', 'Norway', 'China', 'US']
-selcnt = ['Poland', 'Slovakia', 'Germany', 'Czechia', 'Ukraine', 'Belarus', 'Russia']
+selcnt = []
+#selcnt += ['China', 'Korea, South']
+selcnt += ['United Kingdom', 'US']
+selcnt += ['Sweden','Germany','Norway']
+selcnt += ['Italy', 'Spain']
+#selcnt += ['Poland', 'Slovakia', 'Germany', 'Czechia', 'Ukraine', 'Belarus', 'Russia']
 #selcnt = ['Poland', 'Germany','Sweden','US','Norway','Italy','Spain']
-selcnt = ['US', 'Germany', 'Italy', 'Spain', 'Korea, South']
+#selcnt = ['US', 'Germany', 'Italy', 'Spain', 'Korea, South']
 #selcnt = ['Poland', 'Sweden', 'US', 'Norway']
-#selcnt = ['Poland', 'Germany']
+
 
 # %%
 # Prepare the data
 
 def fix_names(c):
     '''
-    Fix differences in naming in datasets
+    Fix differences in naming in population and covid datasets
     '''
     mapa = {'Korea, Rep.':'Korea, South',
             'United States':'US',
@@ -58,48 +58,47 @@ def fix_names(c):
     else :
         return c
 
-
+# Loding population data
 pop = pd.read_csv('https://raw.githubusercontent.com/datasets/population/master/data/population.csv')
 
-selcnt = [fix_names(c) for c in selcnt]
+# Population uses different country names - map it
+pop_cnt = [fix_names(c) for c in selcnt]
+population = {fix_names(c):n for c, _, _, n in 
+                   pop[pop['Country Name'].isin(pop_cnt) & (pop.Year==2018)].values}
 
-populations = {fix_names(c):n for c, _, _, n in 
-                   pop[pop['Country Name'].isin(selcnt) & (pop.Year==2018)].values}
 
-selcnt = [fix_names(c) for c in selcnt]
-countries = list(populations.keys())
-
-# Loading data
+# Loading covid data
 df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv', parse_dates=['Date'])
-
-df = df[df['Country'].isin(countries)]
-
-df['Cases'] = df[['Confirmed', 'Recovered', 'Deaths']].sum(axis=1)
-
-covid = df.pivot(index='Date', columns='Country', values='Cases')
-covid = covid.reset_index('Date')
-covid.set_index(['Date'], inplace=True)
-covid.columns = countries
+# Limit to the selected countries
+df = df[df['Country'].isin(selcnt)]
 
 conf = df.pivot(index='Date', columns='Country', values='Confirmed')
 recov = df.pivot(index='Date', columns='Country', values='Recovered')
-rel = conf.pct_change()
+vict = df.pivot(index='Date', columns='Country', values='Deaths')
+relgr = conf.pct_change()
 
-percapita = covid.copy()
-for country in percapita:
-    percapita[country] = percapita[country]/populations[country]*100000
+# Compute per capita values (per 100_000)
+confpc = conf.copy()
+for country in confpc:
+    confpc[country] = confpc[country]/population[country]*100_000
 
-v = df.pivot(index='Date', columns='Country', values='Deaths')
-vict = v.reset_index('Date')
-vict.set_index(['Date'], inplace=True)
-vict.columns = countries
+victpc = vict.copy()
+for country in victpc:
+    victpc[country] = victpc[country]/population[country]*100_000
 
-victperc = vict.copy()
-for country in victperc:
-    victperc[country] = victperc[country]/populations[country]*100000
+# %% [markdown]
+# ## Relative growth in time
+#
+# This one goes to the heart of exponential growth. If it is exponential its relative growth is constant. If not - we will get linear change or other curve.
+# You can distinquish the exponent by its growth rate much easier.
+#
+# Additionally exponential decay seems to fit the growth rate curves quite well.
 
 # %%
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure(figsize=(10,7))
+
+span = 7
+rel = relgr.rolling(span).mean()
 
 for n, c in enumerate(selcnt):
     m = ~ (np.isnan(rel[c].values) | np.isinf(rel[c].values))
@@ -110,22 +109,46 @@ for n, c in enumerate(selcnt):
             break
     mm = m & (t > s)
     x = arange(rel.index.size)
-    fit = polyfit(x[mm], rel[c].values[mm], 1)
+    fit = polyfit(x[mm], log(rel[c].values[mm]), 1)
     p = plt.plot(rel.index[m], 100*rel[c].values[m], '.', label=c)[0]
     plt.plot(rel.index[mm], 100*rel[c].values[mm], 'o', color=p.get_color())
-    plt.plot(rel.index[mm], 100 * polyval(fit, x[mm]), color=p.get_color())
+    plt.plot(rel.index[mm], 100 * exp(polyval(fit, x[mm])), color=p.get_color())
 
 plt.axhline(5, ls='--', label='approx. critical value (5%)')
 plt.ylim(0,50)
 plt.xlim(pd.Timestamp('2020-03-5'),None)
-plt.title('Daily relative growth of active COVID-19 cases', fontsize = 16, weight = 'bold', alpha = .75)
-plt.ylabel('Relative growth (%)')
+plt.title('Daily relative growth of COVID-19 cases', fontsize = 16, weight = 'bold', alpha = .75)
+plt.ylabel(f'Relative growth (%, rolling {span}-day mean)')
 plt.xlabel('Date')
 plt.legend()
 plt.savefig('relative_growth.png');
 
-# %% jupyter={"source_hidden": true}
-percapitaplot = percapita[selcnt].plot(figsize=(12,8), linewidth=5, logy=LOGY)
+# %% [markdown]
+# ## Trajectory 
+# This one is inspired by the excellent https://aatishb.com/covidtrends/ page
+
+# %%
+plt.figure(figsize=(10,7))
+val = 10*confpc
+span = 7
+gr = val.diff().rolling(span).mean()
+
+for n, c in enumerate(selcnt):
+    m = ~ gr[c].isnull()
+    plt.loglog(val[c][m].values, gr[c][m].values, '-', label=c, lw=2)
+
+plt.xlim(10,None)
+plt.ylim(1,None)
+plt.title('Trajectory of COVID-19 pandemic', fontsize = 16, weight = 'bold', alpha = .75)
+plt.ylabel('Average of daily growth per 1 mln people\n'+
+           f'{span}-day rolling mean')
+plt.xlabel('Cases per 1 mln people')
+plt.legend()
+plt.grid()
+plt.savefig('trajectory.png');
+
+# %%
+percapitaplot = confpc[selcnt].plot(figsize=(12,8), linewidth=5, logy=LOGY)
 percapitaplot.grid(color='#d4d4d4')
 percapitaplot.set_xlabel('Date')
 percapitaplot.set_ylabel('# of Cases per 100,000 People')
@@ -133,19 +156,19 @@ percapitaplot.set_xlim(pd.Timestamp('2020-03-1'),None)
 percapitaplot.set_title("Per Capita COVID-19 Cases", 
                         fontsize = 16, weight = 'bold', alpha = .75);
 
-# %% jupyter={"source_hidden": true}
-vplot = victperc[selcnt].plot(figsize=(12,8), linewidth=5, logy=LOGY)
+# %%
+vplot = victpc[selcnt].plot(figsize=(12,8), linewidth=5, logy=LOGY)
 vplot.grid(color='#d4d4d4')
 vplot.set_xlabel('Date')
 vplot.set_ylabel('# of Deaths per 100,000 People')
 vplot.set_xlim(pd.Timestamp('2020-03-1'),None)
 vplot.set_title("Per Capita deaths due to COVID-19 Cases", fontsize = 16, weight = 'bold', alpha = .75);
 
-# %% jupyter={"source_hidden": true}
-mortplt = (100*victperc[selcnt]/percapita[selcnt]).plot(figsize=(12,8), linewidth=5, logy=False)
+# %%
+mortplt = (100*vict[selcnt]/conf[selcnt]).plot(figsize=(12,8), linewidth=5, logy=False)
 mortplt.grid(color='#d4d4d4')
 mortplt.set_xlim(pd.Timestamp('2020-03-1'),None)
-mortplt.set_ylim(0, 10)
+mortplt.set_ylim(0, 20)
 mortplt.set_xlabel('Date')
 mortplt.set_ylabel('Mortality rate (%)')
 mortplt.set_title('Mortality rate due to COVID-19', fontsize = 16, weight = 'bold', alpha = .75);
@@ -167,8 +190,10 @@ def plleg(c):
         return pl[c]
     else :
         return c
+
+span = 7
+rel = relgr.rolling(span).mean()
     
-ax = gca()
 for n, c in enumerate(selcnt):
     m = ~ (np.isnan(rel[c].values) | np.isinf(rel[c].values))
     t = np.arange(m.size)
@@ -178,21 +203,46 @@ for n, c in enumerate(selcnt):
             break
     mm = m & (t > s)
     x = arange(rel.index.size)
-    fit = polyfit(x[mm], rel[c].values[mm], 1)
-    p = plt.plot(rel.index[m], 100*rel[c].values[m], '.', label=plleg(c))[0]
-    plt.plot(rel.index[mm], 100*rel[c].values[mm], 'o', color=p.get_color())
-    plt.plot(rel.index[mm], 100 * polyval(fit, x[mm]), color=p.get_color())
+    fit = polyfit(x[mm], log(rel[c].values[mm]), 1)
+    p = plt.plot(rel.index[m], 100*rel[c].values[m], '.')[0]
+    plt.plot(rel.index[mm], 100*rel[c].values[mm], 
+             'd' if c=='Poland' else 'o', 
+             color=p.get_color(), label=plleg(c),
+             zorder = 3 if c=='Poland' else 2,
+            )
+    plt.plot(rel.index[mm], 100 * exp(polyval(fit, x[mm])),
+             color=p.get_color(),
+             lw=3 if c=='Poland' else 2,
+             zorder = 3 if c=='Poland' else 2)
 
 plt.axhline(5, ls='--', label='Przybliżony poziom krytyczny (5%)')
 plt.ylim(0,50)
 plt.xlim(pd.Timestamp('2020-03-5'),None)
-plt.title('Dzienny wzrost aktywnych przypadków COVID-19', fontsize = 16, weight = 'bold', alpha = .75)
-plt.ylabel('Dzienny wzrost zakażeń (%)')
+plt.title('Dzienny wzrost przypadków COVID-19', fontsize = 16, weight = 'bold', alpha = .75)
+plt.ylabel(f'Dzienny wzrost zakażeń (%, {span}-dniowa średnia krocząca)')
 plt.xlabel('Data')
 plt.legend()
 plt.savefig('wzrosty_dzienne.png');
 
 # %%
-rel
+plt.figure(figsize=(10,7))
+val = 10*confpc
+span = 7
+gr = val.diff().rolling(span).mean()
 
-# %%
+for n, c in enumerate(selcnt):
+    m = ~ gr[c].isnull()
+    plt.loglog(val[c][m].values, gr[c][m].values, 
+               '-', lw=3 if c=='Poland' else 2, label=plleg(c),
+               zorder = 3 if c=='Poland' else 2)
+
+#plt.axhline(5, ls='--', label='Przybliżony poziom krytyczny (5%)')
+plt.xlim(2,None)
+plt.ylim(0.1,None)
+plt.title('Trajektoria epidemii COVID-19', fontsize = 16, weight = 'bold', alpha = .75)
+plt.ylabel('Średni dzienny przyrost zakażeń na 1 mln mieszkańców\n'+
+           f'{span}-dniowa średnia krocząca')
+plt.xlabel('Liczba przypadków na 1 mln mieszkańców')
+plt.legend()
+plt.grid()
+plt.savefig('trajektoria_covid.png');
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-## My COVID-19 data experiments
+# My COVID-19 data experiments
 [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/jochym/covid_plots/master?filepath=Covid_plots.ipynb)
 
-Feel free to use these examples any way you like according to Creative Commons License.
+Feel free to use these examples any way you like according to Creative Commons License. You can experiment with this using binder, just click the icon above. 
+
+## Examples
 The relative growth rate plot shows the dynamics of the suppression efforts particularly well.
 
 [![Relative growth](relative_growth.png)]()
 
-You can experiment with this using binder, just click the icon above. 
+An the trajectory inspired by https://aatishb.com/covidtrends/ shows the clear signal when each country finally beats the spread of the virus. 
+
+[![Trajectory](trajectory.png)]()
+
