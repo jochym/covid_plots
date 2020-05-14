bf45b76037172d4206772fc20d2c792f7faaae2f
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-05-14 11:12:45 +0200

    Fix wiki update script

--- run_wiki_update.sh
+++ run_wiki_update.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-
+# -*- coding: utf-8 -*-
 
 D=`date`
 SUM="Bot update $D"
 
 echo $SUM
 
+declare -A wikifn
+
+wikifn[wzrosty_dzienne]="Dzienne_zmiany_liczby_aktywnych_przypadków_COVID-19"
+wikifn[trajektoria_covid]="Trajektoria_epidemii_COVID-19"
+wikifn[aktywne_wzrost]="Względne_wzrosty_dzienne_COVID19"
+
 python Covid_plots.py
 
 for fn in wzrosty_dzienne trajektoria_covid aktywne_wzrost ; do
-    pwb.py upload -user:Ptj -family:commons \
+    wfn=${wikifn[$fn]}
+    pwb.py upload -user:Ptj -lang:commons -family:commons \
         -noverify -ignorewarn -keep \
         -summary:"$SUM" \
-        -filename:"${fn}.png" \
+        -filename:"${wfn}.png" \
         ${fn}.png \
-        "Update for $fn"
+        "$SUM"
 done
